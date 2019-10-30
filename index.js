d90147030b17d2f46978530c278fab86b7a43df1
Andy L <andyqly@hotmail.com>
2019-10-30 18:10:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ return "Happy holidays!"
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidaysTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
